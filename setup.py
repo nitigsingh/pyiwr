91ccb2f3d19f40552977c6926bad8b1b5c9efc2a
Tyagi Vaibhav <111271607+VTIITI@users.noreply.github.com>
2023-07-13 19:41:04 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     description='Py-SRT is a helpful open-source library developed by the SIGMA Research Lab at IIT Indore. This powerful tool is designed to effortlessly convert raw DWR (Doppler Weather Radar) files into Py-ART compatible NetCDF files. Furthermore, Py-SRT provides useful tools and visualisation functions to make working with the data easier and more enjoyable.',
     author='Nitig Singh and Vaibhav Tyagi',
     author_email='nitig14rdfsma@gmail.com, phd2201121012@iiti.ac.in and vaibhavtyagi7191@gmail.com',
-    packages=find_packages(),
+    packages=['singledop',],
     install_requires=['arm_pyart', 'numpy'],
 )
