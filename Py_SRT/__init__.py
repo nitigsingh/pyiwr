1dd4e097aaa87c94d60307a08a8bc8c1c25fd157
Nitig Singh <96562403+Space14Mann@users.noreply.github.com>
2023-07-22 15:42:31 +0530

    Update __init__.py

--- Py_SRT/__init__.py
+++ Py_SRT/__init__.py
@@ -2,3 +2,6 @@ from .Py_SRT import dwr2nc
 from .Py_SRT import fread
 from .Py_SRT import mosdac_corrected
 from .Py_SRT import sweeps2xargridnc
+from .graphs import cappi
+from .graphs import cappi_max
+from .graphs import marginal_maxz
