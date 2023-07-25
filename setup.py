3aa7e500cf9cbf00e1f3b689a9c830435a077190
Nitig Singh <96562403+nitigsingh@users.noreply.github.com>
2023-07-25 13:57:07 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Py_SRT',
+    name='pyiwr',
     version='1.0.0',
-    description='Py-SRT is a helpful open-source library developed by the SIGMA Research Lab at IIT Indore. This powerful tool is designed to effortlessly convert raw ISRO DWR (Doppler Weather Radar) data files into Py-ART compatible NetCDF files. Furthermore, Py-SRT provides useful tools and visualisation functions to make working with the data easier and more enjoyable.',
+    description='pyiwr is a helpful open-source library developed by the SIGMA Research Lab at IIT Indore. This powerful tool is designed to effortlessly convert raw ISRO DWR (Doppler Weather Radar) data files into Py-ART compatible NetCDF files. Furthermore, Py-SRT provides useful tools and visualisation functions to make working with the data easier and more enjoyable.',
     author='Nitig Singh & Vaibhav Tyagi',
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     author_email='nitig14rdfsma@gmail.com, phd2201121012@iiti.ac.in and vaibhavtyagi7191@gmail.com',
     long_description=open('README.txt').read(),
-    packages=['Py_SRT',],
+    packages=['pyiwr',],
     install_requires=['arm_pyart', 'numpy', 'xarray'],
-    url='https://github.com/Space14Mann/Py_SRT.git',
+    url='https://github.com/Space14Mann/pyiwr.git',
 )
