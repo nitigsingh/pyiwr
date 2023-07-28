929049662f07c5473fc884030dae69b2acf9521c
Nitig Singh <96562403+nitigsingh@users.noreply.github.com>
2023-07-28 14:59:03 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyiwr',
-    version='1.0.0',
+    version='v1.0.0',
     description='pyiwr is an advanced open-source library developed by researchers at the SIGMA Research Lab at IIT Indore. This powerful tool is designed to effortlessly convert raw ISRO Doppler Weather Radar (DWR) data files and correct MOSDAC radar NetCDF files into Py-ART compatible NetCDF files. pyiwr also provides a range of useful tools and visualization functions to facilitate working with and analyzing weather radar data.',
     author='Nitig Singh & Vaibhav Tyagi',
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
