1e4bd2555d0e75c62a689391e4d6c9cfbd0fc5f2
syedhamidali <hamidsyed37@gmail.com>
2023-09-23 10:27:15 -0400

    updating docs

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = [
 setup(
     author="Nitig Singh, Vaibhav Tyagi, Hamid Ali Syed",
     author_email="nitig14rdfsma@gmail.com",
-    python_requires=">=3.6",
+    python_requires=">=3.9",
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
