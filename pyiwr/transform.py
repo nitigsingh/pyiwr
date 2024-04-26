8d67cca95b81b739a8bc47682ef1a182c3644f8b
Nitig Singh <96562403+nitigsingh@users.noreply.github.com>
2024-04-27 02:39:01 +0530

    Update transform.py

--- pyiwr/transform.py
+++ pyiwr/transform.py
@@ -22,19 +22,24 @@ analyzing weather radar data.
 
 import os
 import tempfile
+import datetime as dt
+from netCDF4 import Dataset
 import xarray as xr
-
+import numpy as np
+from pyart.config import get_metadata
 import pyart
 import pyart.map
 
 
 from .utilities import (
     raw2object,
-    read,
+    fread,
     raw_product_list,
     raw_reshape_stack,
     update_xarray_dataset,
     extract_start_time,
+    make_grid,
+    sorting_key
 )
 
 # "raw2nc" function takes in any raw dual-pol .dwr file and restructures it into a radar object to be visualized by pyiwr and also makes it compatible with Py-ART
@@ -252,3 +257,256 @@ def sweeps2gridnc(
             "done successfully",
         )
         return xg0
+
+def sweeps2mergednc(path_string,
+    start_index = 0,
+    end_index=None,
+    scan_type="B",
+    dualpol=False,
+    gridder=False,
+    save_file=False,
+                ):
+    
+    
+    """
+    Aggregates data to cfradial1 data.
+    input_dir(str): Enter path of single sweep data directory,
+    output_dir(str): Enter the path for output data,
+    scan_type(str): "B", "C". B is for short range PPI,
+                & C is for long range PPI.
+    dualpol(bool): True, False. (If the data contains
+                dual-pol products e.g., ZDR, RHOHV),
+    gridder(bool): True, False,
+    nf(int): Number of files to group together
+    """
+       
+    # List of file names with the .nc extension
+    folder_path = glob.glob(os.path.join(path_string, "*nc*"))
+    startt = dt.datetime.now()
+    if end_index is None:
+        end_index = len(folder_path) - 1
+    files = folder_path[start_index:end_index+1]
+
+    files = sorted(files, key=sorting_key)
+    nf = len(files)
+
+    print("Number of files: ", nf)
+    bb = list()
+    if scan_type == "B":
+        if nf is None:
+            nf = 10
+        for i in range(0, len(files), nf):
+            bb.append(files[i : i + nf])
+    elif scan_type == "C":
+        if nf is None:
+            nf = 2
+        for i in range(0, len(files), nf):
+            bb.append(files[i : i + nf])
+    print(f"Total no. of output files: {len(bb)}.")
+    print("Merging all scans into one Volume")
+              
+    for i in range(0, len(bb)):
+        en = []
+        a1 = []
+        t1 = []
+        e1 = []
+        Z1 = []
+        T1 = []
+        V1 = []
+        W1 = []
+        ZDR1 = []
+        KDP1 = []
+        PHIDP1 = []
+        SQI1 = []
+        RHOHV1 = []
+        HCLASS1 = []
+        nyquist = []
+        unambigrange = []
+        for j in range(0, nf):
+            ds = Dataset(bb[i][j])
+            az = ds.variables["radialAzim"][:]
+            time = ds.variables["radialTime"][:]
+            ele = ds.variables["radialElev"][:]
+            Z = ds.variables["Z"][:]
+            # T = ds.variables['T'][:]
+            V = ds.variables["V"][:]
+            W = ds.variables["W"][:]
+            EN = ds.variables["elevationNumber"][:]
+            a1.extend(az)
+            t1.extend(time)
+            e1.extend(ele)
+            Z1.extend(Z)
+            # T1.extend(T)
+            V1.extend(V)
+            W1.extend(W)
+            en.append(EN)
+            nyquist.append(ds.variables["nyquist"][:])
+            unambigrange.append(ds.variables["unambigRange"][:])
+            if dualpol:
+                if "ZDR" in ds.variables:
+                    ZDR = ds.variables["ZDR"][:]
+                    ZDR1.extend(ZDR)
+
+                if "PHIDP" in ds.variables:
+                    PHIDP = ds.variables["PHIDP"][:]
+                    PHIDP1.extend(PHIDP)
+
+                if "KDP" in ds.variables:
+                    KDP = ds.variables["KDP"][:]
+                    KDP1.extend(KDP)
+
+                if "SQI" in ds.variables:
+                    SQI = ds.variables["SQI"][:]
+                    SQI1.extend(SQI)
+
+                if "RHOHV" in ds.variables:
+                    RHOHV = ds.variables["RHOHV"][:]
+                    RHOHV1.extend(RHOHV)
+
+                if "HCLASS" in ds.variables:
+                    HCLASS = ds.variables["HCLASS"][:]
+                    HCLASS1.extend(HCLASS)
+
+
+        fname = os.path.basename(bb[i][0]).split(".nc")[0]
+
+        radar = pyart.testing.make_empty_ppi_radar(
+            ds.dimensions["bin"].size, ds.dimensions["radial"].size * nf, 1
+        )
+        radar.nsweeps = nf
+        radar.time["data"] = np.array(t1)
+        # 'seconds since 1970-01-01T00:00:00Z'
+        radar.time["units"] = ds.variables["radialTime"].units
+        radar.latitude["data"] = np.array([ds.variables["siteLat"][:]])
+        radar.longitude["data"] = np.array([ds.variables["siteLon"][:]])
+        radar.altitude["data"] = np.array([ds.variables["siteAlt"][:]])
+        radar.range["data"] = np.arange(
+            0,
+            ds.dimensions["bin"].size * ds.variables["gateSize"][:].data,
+            int(ds.variables["gateSize"][:].data),
+        )
+
+        radar.fixed_angle["data"] = ds.variables["elevationList"]
+
+        radar.sweep_number["data"] = np.array(en)
+
+        radar.sweep_start_ray_index["data"] = np.arange(
+            0, ds.dimensions["radial"].size * nf, ds.dimensions["radial"].size
+        )
+
+        radar.sweep_end_ray_index["data"] = np.arange(
+            ds.dimensions["radial"].size - 1,
+            ds.dimensions["radial"].size * nf,
+            ds.dimensions["radial"].size,
+        )
+
+        radar.azimuth["data"] = np.ma.array(a1)
+        radar.elevation["data"] = np.ma.array(e1)
+        radar.metadata["instrument_name"] = fname[:3]
+        radar.init_gate_altitude()
+        radar.init_gate_longitude_latitude()
+        ref_dict = get_metadata("reflectivity")
+        ref_dict["data"] = np.ma.array(Z1)
+        ref_dict["units"] = "dBZ"
+        VEL_dict = get_metadata("velocity")
+        VEL_dict["data"] = np.ma.array(V1)
+        VEL_dict["units"] = "m/s"
+        W_dict = get_metadata("spectrum_width")
+        W_dict["data"] = np.ma.array(W1)
+        W_dict["units"] = "m/s"
+        radar.instrument_parameters = {}
+        radar.instrument_parameters["nyquist_velocity"] = {
+            "units": "m/s",
+            "comments": "Unambiguous velocity",
+            "meta_group": "instrument_parameters",
+            "long_name": "Nyquist velocity",
+        }
+        radar.instrument_parameters["nyquist_velocity"]["data"] = np.ma.array(nyquist)
+        radar.instrument_parameters["unambiguous_range"] = {
+            "units": "meters",
+            "comments": "Unambiguous range",
+            "meta_group": "instrument_parameters",
+            "long_name": "Unambiguous range",
+        }
+        radar.instrument_parameters["unambiguous_range"]["data"] = np.ma.array(
+            unambigrange
+        )
+
+        radar.fields = {"REF": ref_dict, "VEL": VEL_dict, "WIDTH": W_dict}
+
+        if dualpol:
+            ZDR_dict = get_metadata("differential_reflectivity")
+            ZDR_dict["units"] = "dB"
+            if "ZDR" in ds.variables:
+                ZDR_dict["data"] = np.ma.array(ZDR1)
+                radar.fields["ZDR"] = ZDR_dict
+            
+            PHIDP_dict = get_metadata("differential_phase")
+            PHIDP_dict["units"] = "degrees"
+            if "PHIDP" in ds.variables:
+                PHIDP_dict["data"] = np.ma.array(PHIDP1)
+                radar.fields["PHIDP"] = PHIDP_dict
+            
+
+            KDP_dict = get_metadata("specific_differential_phase")
+            KDP_dict["units"] = "degrees/km"
+            if "KDP" in ds.variables:
+                KDP_dict["data"] = np.ma.array(KDP1)
+                radar.fields["KDP"] = KDP_dict
+
+
+            RHOHV_dict = get_metadata("cross_correlation_ratio")
+            RHOHV_dict["units"] = "unitless"
+            if "RHOHV" in ds.variables:
+                RHOHV_dict["data"] = np.ma.array(RHOHV1)
+                radar.fields["RHOHV"] = RHOHV_dict 
+            
+
+            SQI_dict = get_metadata("normalized_coherent_power")
+            SQI_dict["units"] = "unitless"
+            if "SQI" in ds.variables:
+                SQI_dict["data"] = np.ma.array(SQI1)
+                radar.fields["SQI"] = SQI_dict
+            
+
+            HCLASS_dict = get_metadata("radar_echo_classification")
+            HCLASS_dict["units"] = "unitless"
+            if "HCLASS" in ds.variables:
+                HCLASS_dict["data"] = np.ma.array(HCLASS1)
+                radar.fields["HCLASS"] = HCLASS_dict
+
+              
+        # Save the updated dataset to a new netCDF file if specified
+        if save_file:
+            # Create the "corrected" subdirectory if it doesn't exist
+            merged_dir = os.path.join(os.path.dirname(path), "merged")
+            os.makedirs(merged_dir, exist_ok=True)
+
+            # Specify the new file path
+            new_file_name = f"merged_cfrad_{fname}.nc"
+            new_file_path = os.path.join(merged_dir, new_file_name)
+            pyart.io.write_cfradial(new_file_path, radar, format="NETCDF4")
+              
+            if gridder:
+                grid = make_grid(radar)
+                new_file_name = f"merged_grid_{fname}.nc"
+                new_file_path = os.path.join(merged_dir, new_file_name)
+                pyart.io.write_grid(new_file_path, grid)
+                del radar, grid
+            else:
+                pass
+        else:
+            if gridder:
+                grid = make_grid(radar)
+                # Save the corrected xarray.Dataset to a temporary in-memory file
+                with tempfile.NamedTemporaryFile(suffix=".nc", delete=False) as tmp_file:
+                    pyart.io.write_grid(tmp_file.name, grid)  
+                grids = pyart.io.read_grid(tmp_file.name)
+                
+                # Delete the temporary in-memory file
+                os.remove(tmp_file.name)                
+                
+                return grids
+            else:
+                return radar
+    print("Data merging done \nTotal Time Elapsed: ", dt.datetime.now() - startt)
